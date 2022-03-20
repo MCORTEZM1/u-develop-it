0dfc7f041a5ffb9f1c1d05b5f159300ccc9134cd
Michael Cortez-Mejia <you@example.com>
2022-03-20 15:56:15 -0700

    added SQL functions in server to introduce express routes

--- server.js
+++ server.js
@@ -20,12 +20,43 @@ const db = mysql.createConnection({
    console.log('Connected to the election database.')
 );
 
-// db object uses query method which runs SQL query and executes the callback 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    // err = is an error response ofc; null if no errs. 
-    // rows is the database query response
-    console.log(rows);
-});
+// // db object uses query method which runs SQL query and executes the callback 
+// =========== UNCOMMENT AS YOU CREATE EXPRESS ROUTES ===================
+
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     // err = is an error response ofc; null if no errs. 
+//     // rows is the database query response
+//     console.log(rows);
+// });
+
+// // GET a single candidate 
+// db.query(`SELECT * FROM candidates WHERE id = 1;`, (err, row) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+
+// // Delete a candidate 
+// db.query(`DELETE FROM candidates WHERE id = ?`, 2, (err, result) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result)
+// });
+
+// // Create a candidate 
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//                 VALUES (?,?,?,?)`;
+// const params = [2, 'Virginia', 'Woolf', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 
 // Default response for any other request (Not found)== placement is important becuase it is a catchall route! 
